94f2dd6e23f6c547fb4af6d40bec93da744e2625
Vladislav Nevsky <vnevskii650@gmail.com>
2025-11-25 09:57:26 +0300

    add: gunicorn

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:8000"
+workers = 4
+threads = 2
+timeout = 120
+max_requests = 1000
+max_requests_jitter = 100
--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,14 @@
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY", "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$"
-)
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ("localhost,127.0.0.1,backend")).split(',')
-
-STATIC_ROOT = "/app/collected_static"
-MEDIA_ROOT = "/app/media"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ("localhost,127.0.0.1,backend")).split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
